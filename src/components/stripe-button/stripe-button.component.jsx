224919caf7c5f0c86f0594b0b5203a3c73b06af8
Nam Pham <nam.pham@team.enouvo.com>
2022-02-17 09:10:06 +0700

    add styled components

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+    const priceForStripe = price * 100;
+    const publishableKey = 'pk_test_51KTml8LMw4RAnkbrtbky0ihiNXR47EjO27XYNyG9HfIIkMDdhsKXaKYiZY52wdYVniwcYvgxJaflTHCl5y4AeVZd0026Lhx1RZ';
+
+    const onToken = token => {
+        console.log(token)
+        alert('Payment Successful')
+    }
+
+    return (
+        <StripeCheckout
+            label='Pay Now'
+            name='CRWN Clothing Ltd.'
+            billingAddress
+            shippingAddress
+            image='https://svgshare.com/i/CUz.svg'
+            description={`Your goal is $${price}`}
+            amount={priceForStripe}
+            panelLabel='Pay Now'
+            token={onToken}
+            stripeKey={publishableKey}
+        />
+    )
+}
+
+export default StripeCheckoutButton;
